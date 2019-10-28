1b356112dfdf0d831903736840ae9520ce238c12
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-10-28 15:57:39 -0400

    adding camera script

--- Assets/Scripts/CameraLerpToTransform.cs
+++ Assets/Scripts/CameraLerpToTransform.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class CameraLerpToTransform : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform camTarget;
+    public float trackingSpeed;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    void FixedUpdate()
+    {
+        if (camTarget!= null)
+        {
+            var newPos = Vector2.Lerp(transform.position, camTarget.position, Time.deltaTime * trackingSpeed);
+
+            var CamPosition = new Vector3(newPos.x, newPos.y, -10f);
+            var v3 = CamPosition;
+
+            var clampx = Mathf.Clamp(v3.x, minX, maxX);
+
+            var clampy = Mathf.Clamp(v3.y, minY, maxY);
+
+            transform.position = new Vector3(clampx, clampy, -10f);
+        }
+    }
 }
