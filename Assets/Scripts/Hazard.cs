35722c9cd3027475b59f42b644b76df5f20f00f2
snick harrison <harrisonnick16@gmai.com>
2019-11-11 01:48:58 -0500

    adding death and making hazards active

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+    private SpriteRenderer spriteRenderer;
+
+
+     void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+     void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.transform.tag=="Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if(audioSource != null && deathClip !=null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+
+            Instantiate(playerDeathPrefab, coll.contacts[0].point, Quaternion.identity);
+            spriteRenderer.sprite = hitSprite;
+
+            Destroy(coll.gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+
+    public float rotationsPerMinute = 640f;
+
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationsPerMinute * Time.deltaTime, Space.Self);
+    }
+}
