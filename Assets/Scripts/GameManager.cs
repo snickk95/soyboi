954c2cbef9f72fe18379c840f9dd37df5ceca059
snick harrison <harrisonnick16@gmai.com>
2019-11-11 02:14:30 -0500

    adding a game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+     void Awake()
+    {
+        if(instance==null)
+        {
+                instance = this;
+        }
+        else if(instance!=this)
+        {
+            Destroy(gameObject);
+        }
+
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void RestartLevel(float delay)
+    {
+        StartCoroutine(RestartLevelDelay(delay));
+    }
+
+    private IEnumerator RestartLevelDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Game");
+    }
+}
--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -30,6 +30,8 @@ public class Hazard : MonoBehaviour
             spriteRenderer.sprite = hitSprite;
 
             Destroy(coll.gameObject);
+
+            GameManager.instance.RestartLevel(1.25f);
         }
     }
 
