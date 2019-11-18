770ae79d41561d110ee536b17ee1aad8c94c907d
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-11-18 15:32:13 -0500

    adding text object and save time script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class GameManager : MonoBehaviour
 {
@@ -47,5 +52,48 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene("Level 1");
+    }
+
+    public List<PlayerTimeEntry> LoadPreviousTimes()
+    {
+        //use try catch to attempt to load saved time entries with a combontion of the players name and the app persistent data path function
+        try
+        {
+            var scoreFile = Application.persistentDataPath + "/" + playerName + "_times.dat";
+            using (var stream = File.Open(scoreFile, FileMode.Open))
+            {
+                var bin = new BinaryFormatter();
+                var times = (List<PlayerTimeEntry>)bin.Deserialize(stream);
+                return times;
+            }
+        }
+        //if the try fails the catch will find the error in deserialization and display the error message 
+        catch(IOException ex)
+        {
+            Debug.Log("could not load previous time for: " + playerName + ".Exception: " + ex.Message);
+            return new List<PlayerTimeEntry>();
+        }
+
+    }
+
+    public void SaveTime(decimal time)
+    {
+        //when savinf times load the previous time first
+        var times = LoadPreviousTimes();
+        //create a new p-layer time object 
+        var newTime = new PlayerTimeEntry();
+
+        newTime.entryDate = DateTime.Now;
+        newTime.time = time;
+
+        //create a binary formatter to serialize the player time data
+        var bFormatter = new BinaryFormatter();
+        var filePath = Application.persistentDataPath + "/" + playerName + "_times.dat";
+
+        using (var file = File.Open(filePath, FileMode.Create))
+        {
+            times.Add(newTime);
+            bFormatter.Serialize(file, times);
+        }
     }
 }
